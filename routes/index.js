5fdc8cedbd62a88d8fc8ea5b23b7a5e62d834c10
Brandon Mellus <mellusbrandon@gmail.com>
2021-09-30 13:22:47 -0700

    Add view routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const path = require('path');
 
-// const apiRouter = require('./api');
+const apiRouter = require('./api');
 
-// router.use('/api', apiRouter);
+router.use('/api', apiRouter);
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
 
 module.exports = router;
